2c61a61669641ac7521742e034cea9c1ba82c4bb
K4speeer <dri.taha24@gmail.com>
2022-04-12 03:06:22 +0300

    Iploading progress

---
+++ Interactive Rating Component/scripts/index.js
@@ -0,0 +1,11 @@
+$("btn").click(function(){
+  console.log(this.getAttribute("id"));
+})
+
+// var submitted = false;
+
+// $("submit").click(function(){
+//   submitted = true;
+//   console.log(submitted);
+// })
+
